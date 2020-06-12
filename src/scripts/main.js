771fa968559c82b91a6c27cb3d25f8f3def222d8
viktorcpp <viktor.cpp.root@gmail.com>
2020-06-12 09:26:28 +0300

    First commit after repo restoring

---
+++ src/scripts/main.js
@@ -0,0 +1,27 @@
+
+'use strict';
+
+function Main(e)
+{
+    //
+
+} // Main
+
+
+function OnLoaded(e)
+{
+    //
+
+} // OnLoaded
+
+
+function OnResize(e)
+{
+    //
+
+} // OnResize
+
+
+window.addEventListener( "DOMContentLoaded", Main );
+window.addEventListener( "load",             OnLoaded );
+window.addEventListener( "resize",           ()=>{ OnResize(); } );
